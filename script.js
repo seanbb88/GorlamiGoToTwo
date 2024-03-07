0bab6c470e621ec737d1ca872c40ea59561ee10f
Sean Brown <32752782+seanbb88@users.noreply.github.com>
2024-03-06 20:31:24 -0500

    init

--- script.js
+++
@@ -1,6 +0,0 @@
-// Create a new JavaScript file named script.js
-
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("The page is loaded!");
-    // You can add more JavaScript code here to enhance your website.
-});
